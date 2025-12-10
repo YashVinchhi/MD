20d5606810407ab0bbf5ef732f765fddbdd9cf85
Yash H Vinchhi <yashhvinchhi@gmail.com>
2025-12-10 14:07:47 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import MarkFlowClient from '@/components/markflow/MarkFlowClient';
+import dynamic from 'next/dynamic';
+
+const MarkFlowClient = dynamic(() => import('@/components/markflow/MarkFlowClient'), { ssr: false });
 
 export default function Home() {
   return <MarkFlowClient />;
